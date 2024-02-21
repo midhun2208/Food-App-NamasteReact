c7232354c94fcc001a3484f5456276b0e81c51e7
midhun2208 <midhun.sm343@gmail.com>
2024-02-21 15:48:31 +0530

    Food-App-Namaste React

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -60,12 +60,10 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            
             const filteredList = listOfRest.filter(
               (res) => res.info.avgRating > 4.3
             );
             setFilterRest(filteredList);
-           
           }}
         >
           {" "}
