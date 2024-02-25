65767a68f5b47781a888860535ec9c9c619c5ce8
midhun2208 <midhun.sm343@gmail.com>
2024-02-25 15:46:16 +0530

    Episode 08

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 
 // passed a porp name from the About Component
-const User = ({name}) => {
+const User = ({ name }) => {
+  //to create state variable in  functional component
+const [count] = useState(0)
+
   return (
     <div className="user-card">
+      <h1>Count1 : {count}</h1>
       <h1>Name : {name}</h1>
       <h2>Location : Kerala</h2>
       <h3>Contact : midhun.sm343@gmail.com</h3>
--- src/Components/UserClass.jsx
+++ src/Components/UserClass.jsx
@@ -3,14 +3,21 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
 
     return (
       <div className="user-card">
+        <h1>Count : {count}</h1>
+        <h1>Count2 : {count2}</h1>
         <h1>Name : {name}</h1>
         <h2>Location : {location}</h2>
         <h3>Contact : midhun.sm343@gmail.com</h3>
