05075c68eae574880a25319c9a68c56c6ff2f88b
midhun2208 <midhun.sm343@gmail.com>
2024-03-23 22:17:59 +0530

    Contact for testing

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,12 @@
 const Contact = () => {
     return(
         <div>
-            <h1>Contact page </h1>
+            <h1 className="font-bold text-3xl p-4 m-4">Contact page </h1>
+            <form action="">
+                <input type="text" placeholder="name" className="border border-black m-2 p-2" />
+                <input type="text" placeholder="message" className="border border-black m-2 p-2" />
+                <button className="border border-black m-2 p-2 rounded-lg bg-gray-200">Submit</button>
+            </form>
         </div>
     )
 };
